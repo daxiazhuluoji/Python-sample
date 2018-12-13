88040dee5c19f8ec510e33e237635aae81917243
lidianjun <lidianjun@yeah.net>
2018-12-13 13:49:34 +0800

    first commit

---
+++ basic_operation.py
@@ -0,0 +1,14 @@
+inputchar = 0
+while inputchar != 'a':
+    inputchar = input('please input a number\n')
+    if inputchar == 'a':
+        print('you are right\n')
+    else:
+        print('sorry\n')
+a = 1
+b = 2
+type(a)
+type(b)
+print(type(b + a))
+print(a+b+b)
+print(isinstance(3,int))
