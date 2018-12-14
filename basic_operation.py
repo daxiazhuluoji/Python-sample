17bb06a3e9a55ea728c322ccd529175c46a0a73a
ldj <dianjun.li@huntersun.com.cn>
2018-12-14 12:03:30 +0800

    do some string sample

--- basic_operation.py
+++ basic_operation.py
@@ -1,10 +1,4 @@
-inputchar = 0
-while inputchar != 'a':
-    inputchar = input('please input a number\n')
-    if inputchar == 'a':
-        print('you are right\n')
-    else:
-        print('sorry\n')
+#type
 a = 1
 b = 2
 type(a)
@@ -12,3 +6,25 @@ type(b)
 print(type(b + a))
 print(a+b+b)
 print(isinstance(3,int))
+
+#string
+srcstr = "abcd"
+secondstr = "12345"
+newstr = srcstr.join(secondstr)
+print(srcstr)
+print(secondstr)
+print(newstr)
+
+
+#shile and switch
+#inputchar = 0
+#while inputchar != 'a':
+#    inputchar = input('please input a number\n')
+#    if inputchar == 'a':
+#        print('you are right\n')
+#    else:
+#        print('sorry\n')
+
+#5.3.2
+
+
